df94afb730bb167aa6800b89049b704e695aa62b
GodOfOni <118010221+GodOfOni@users.noreply.github.com>
2025-06-10 21:15:24 +0200

    update 1

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-// script.js
+function openModal(imgElement) {
+  const modal = document.getElementById("myModal");
+  const modalImg = document.getElementById("modal-img");
+  const captionText = document.getElementById("caption");
 
-// JavaScript functionality can be added here if needed
-// For example, interactive features or dynamic content loading
-// Pobranie modal
-var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+  modalImg.src = imgElement.src;
+  captionText.innerText = imgElement.alt;
+}
 
-// Pobranie obrazka w modal
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-
-// Pobranie wszystkich miniatur
-var thumbnails = document.querySelectorAll('.thumbnail');
-
-// Dodanie event listenera do każdej miniatury
-thumbnails.forEach(function(thumbnail) {
-    thumbnail.addEventListener('click', function() {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    });
-});
-
-// Pobranie elementu zamykającego
-var span = document.getElementsByClassName("close")[0];
-
-// Zamknięcie modal przy kliknięciu na (x)
-span.onclick = function() {
-    modal.style.display = "none";
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
 }
